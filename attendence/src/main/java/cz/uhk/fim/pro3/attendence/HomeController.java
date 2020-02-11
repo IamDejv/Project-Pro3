95facf6d0c0796bfa9bd9d87db554367d8fc9bb6
David Kalianko <david.kalianko@quanti.cz>
2020-02-11 14:54:24 +0100

    Add Home Controller

---
+++ attendence/src/main/java/cz/uhk/fim/pro3/attendence/HomeController.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.pro3.attendence;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "Hello";
+    }
+}
