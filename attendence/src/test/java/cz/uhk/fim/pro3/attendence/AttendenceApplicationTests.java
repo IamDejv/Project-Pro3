82a90b65b84daeea4c19b10a8863b1f501e29c8c
IamDejv <56268618+IamDejv@users.noreply.github.com>
2020-02-11 14:18:13 +0100

    First Commit

---
+++ attendence/src/test/java/cz/uhk/fim/pro3/attendence/AttendenceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.uhk.fim.pro3.attendence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
