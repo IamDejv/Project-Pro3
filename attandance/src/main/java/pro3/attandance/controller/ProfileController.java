15f4d57c87aba0fb99f8b0b79498e1f522abc814
David Kalianko <david.kalianko@quanti.cz>
2020-03-29 13:13:47 +0200

    Attendee profile

--- attandance/src/main/java/pro3/attandance/controller/ProfileController.java
+++ attandance/src/main/java/pro3/attandance/controller/ProfileController.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import pro3.attandance.model.Attendee;
 import pro3.attandance.model.User;
+import pro3.attandance.services.AttendeeService;
 import pro3.attandance.services.UserService;
 
 import java.util.Optional;
@@ -17,12 +19,15 @@ public class ProfileController {
 
     private UserService userService;
 
-    public ProfileController(UserService userService) {
+    private AttendeeService attendeeService;
+
+    public ProfileController(UserService userService, AttendeeService attendeeService) {
         this.userService = userService;
+        this.attendeeService = attendeeService;
     }
 
-    @GetMapping("/profil/{id}")
-    public String profile(@PathVariable("id") int id, Model model) {
+    @GetMapping("/profil/user/{id}")
+    public String userProfile(@PathVariable("id") int id, Model model) {
 
         Optional<User> userOpt = userService.getById(id);
         User user;
@@ -30,6 +35,17 @@ public class ProfileController {
         model.addAttribute("personid", id);
         model.addAttribute("user", user);
         System.out.println(user.getUsername());
+        return "profile/index";
+    }
+
+    @GetMapping("/profil/attendee/{id}")
+    public String attendeeProfile(@PathVariable("id") int id, Model model) {
+
+        Optional<Attendee> attendeeOpt = attendeeService.getById(id);
+        Attendee attendee;
+        attendee = attendeeOpt.orElse(null);
+        model.addAttribute("personid", id);
+        model.addAttribute("attendee", attendee);
         return "profile/index";
     }
 }
