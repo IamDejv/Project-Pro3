e17ad1c07fcc30c8f257cd190c99b705abfeb27c
IamDejv <kaliankodavid@gmail.com>
2020-04-25 00:57:58 +0200

    Fixes

--- attandance/src/scripts/attToTrainings.sh
+++ attandance/src/scripts/attToTrainings.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 atts="files/attendees.csv"
-url="http://localhost:8081/dochazka/assignAttendee"
+url="http://localhost:8081/api/v1/attendance/assignAttendee"
 
 trainingsID=("1745" "1746" "1743" "1744" "1747" "1742" "1749")
+
+a=0
 
 while read -r line; do
     IFS=',' read -r -a items <<< ${line}
@@ -11,9 +13,14 @@ while read -r line; do
     then
         data="{
                 \"attendeeid\" : ${items[0]},
-                \"trainingid\": ${trainingsID[0]}
+                \"trainingid\": ${trainingsID[${a}]}
                 }"
-        echo \"curl\" -d ${data} ${url} >> "files/output.txt"
+        curl -d "${data}" -H 'Content-type: application/json' ${url}
+    fi
+    a=$(( ${a} + 1 ))
+    if [[ ${a} -gt 6 ]]
+    then
+        a=0
     fi
 
 done <<< $(cat ${atts})
